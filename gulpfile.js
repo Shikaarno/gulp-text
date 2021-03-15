be20e386aba05270d84ae204d35b8f8521670e71
Shikaarno <union_contact@rambler.ru>
2021-03-16 00:24:05 +0300

    add gulp constructor with lessToMinCss task

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
 const { parallel, series, src, dest } = require('gulp');
 const less = require('gulp-less');
 const sourcemaps = require('gulp-sourcemaps');
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
 
 
-function less_compile() {
-  return src('./src/less/template.less')
+function lessToMinCss() {
+  return src('./src/less/temp.less')
     .pipe(less())
+    .pipe(cssmin())
+    .pipe(rename('style.min.css'))
     .pipe(sourcemaps.init())
     .pipe(sourcemaps.write("/"))
     .pipe(dest('./css'));
 };
 
-exports.less = less_compile;
+exports.less = lessToMinCss;
